1ff3a19acebb5554925898944bdfb16108ecffb8
Nate Dobbs <ndobbs@hc1.com>
2014-12-03 14:14:54 -0500

    layed down folder structure, whipped up a quick bootstrap script to test POC

---
+++ inventory/bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/env bash
+# Written by Nate Dobbs 12/03/2014
+# script is used to bootstrap a machine for an ansible configuration
+# currently only supports RHEL-based machines ( Redhat, Fedora, CentOS )
+
+set -e
+
+# Download and install Ansible
+if [[ ! -x /usr/local/bin/ansible ]]; then
+    echo "Info   | Install   | Ansible"
+    yum install ansible -y
+fi
+
+# Ensure git is installed
+if [[ ! -x /usr/local/bin/git ]]; then
+    echo "Info  | Install  | git"
+    yum install git -y
+fi
+
+# Modify the PATH
+# This should be subsequently updated in shell settings
+export PATH=/usr/local/bin:$PATH
+
+# Clone the remote ansible-role-zoneminder repo
+git clone https://github.com/ndobbs/ansible-role-zoneminder.git ./roles
---
+++ inventory/clone.sh
@@ -0,0 +1,2 @@
+# Script is used to clone the remote ansible-role-zoneminder repo locally
+# 
